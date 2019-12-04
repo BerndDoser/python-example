326e0ab3d79b0eb055fb1823e02ccb1bd80166f0
Bernd Doser <bernd.doser@h-its.org>
2019-12-04 15:26:37 +0100

    solution

---
+++ fibomath/__init__.py
@@ -0,0 +1,1 @@
+import fibomath.fibo
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import setuptools
 
 setuptools.setup(
-    name="example-pkg-your-username",
-    version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
+    name="python-example",
+    version="1.0",
+    author="Bernd Doser",
+    author_email="bernd.doser@h-its.org",
     description="A small example package",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/bernddoser/python-example",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
